840ac61ebbd4d4fcdd656f92b462d52dbe19cc72
giraffe1217 <51290300+giraffe1217@users.noreply.github.com>
2020-06-13 17:16:01 +0800

    update

--- src/test/ControlRedis.java
+++ src/test/ControlRedis.java
@@ -10,6 +10,8 @@ import java.util.Set;
 public class ControlRedis {
     private static Jedis jedis;
 
+    //region 初始化及测试
+
     //创建连接
     private static void InitialJedis()
     {
@@ -19,8 +21,25 @@ public class ControlRedis {
     //关闭连接
     private static void CloseJedis()
     {
-        jedis.close();
-        jedis = null;
+        if (jedis!= null)
+        {
+            jedis.close();
+            jedis = null;
+        }
+    }
+
+    //测试 helloworld！
+    public static String SayHelloWorld() throws Exception
+    {
+        InitialJedis();
+
+        jedis.set("test", "HelloWorld!");
+        String value = jedis.get("test");
+
+        jedis.del("test");
+
+        CloseJedis();
+        return value;
     }
 
     //清空数据库中的所有内容
@@ -30,6 +49,10 @@ public class ControlRedis {
         jedis.flushAll();
         CloseJedis();
     }
+
+    //endregion
+
+    //region 注册与登录
 
     //TODO 买家进行账户和密码的存储 (注册)
     public static void CustomerSignIn(String userName,String password) throws Exception
@@ -98,25 +121,168 @@ public class ControlRedis {
         return flag;
     }
 
+    //endregion
 
-    //测试 helloworld！
-    public static String SayHelloWorld() throws Exception
+    //region 卖家管理商品
+
+    // TODO 增加一个图书商品  (用户名,书名，编号，类型，价格，ISBN，摘要，出售种类，剩余数量)   其中 bookNum 不允许相同
+    public static boolean StoreOneGood(String userName, String bookName,String bookNum,String bookType,String price,
+                                       String ISBN,String summary,String sellType,String remainNum)
     {
         InitialJedis();
 
-        jedis.set("test", "HelloWorld!");
-        String value = jedis.get("test");
+        // 如果当前存在 userName 这个集合 那么就检查一下有没有当前商品
+        if(jedis.exists(userName))
+        {
+            // bookNum 重复   操作失败
+            if(jedis.sismember(userName,userName + bookNum))
+            {
+                return false;
+            }
+        }
 
-        jedis.del("test");
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("bookName",bookName);
+        map.put("bookNum",bookNum);
+        map.put("bookType",bookType);
+        map.put("price",price);
+        map.put("ISBN",ISBN);
+        map.put("summary",summary);
+        map.put("sellType",sellType);
+        map.put("remainNum",remainNum);
+
+        // 由于 不同的卖家 对应不同的 出售商品列表   需要一个集合 userName 来存储 它的所有商品
+        // 每个商品对应 一个哈希表 userName + bookNum 来保存当前商品的所有信息
+        jedis.hmset(userName + bookNum,map);
+        jedis.sadd(userName,userName + bookNum);
 
         CloseJedis();
-        return value;
+        return true;
     }
+
+    // TODO 增加一个图书商品   重载
+    public static boolean StoreOneGood(String userName, Map<String,String> map)
+    {
+        InitialJedis();
+
+        // 由于 不同的卖家 对应不同的 出售商品列表   需要一个集合 userName 来存储 它的所有商品
+        // 每个商品对应 一个哈希表 userName + bookNum 来保存当前商品的所有信息
+        jedis.hmset(userName + map.get("bookNum"), map);
+        jedis.sadd(userName,userName + map.get("bookNum"));
+
+        CloseJedis();
+        return true;
+    }
+
+    // TODO 删除一个图书商品  (用户名,书名，编号，类型，价格，ISBN，摘要，出售种类，剩余数量)   其中 bookNum 不允许相同
+    public static boolean RemoveOneGood(String userName,String bookNum)
+    {
+        InitialJedis();
+
+        // 如果当前存在 userName 这个集合 那么就检查一下有没有当前商品
+        if(jedis.exists(userName))
+        {
+            // 不存在当前bookNum   操作失败
+            if(!jedis.sismember(userName,userName + bookNum))
+            {
+                return false;
+            }
+        }
+        else {
+            //如果当前不存在 userName 这个集合   操作失败
+            return false;
+        }
+
+        //删除  如果remainNum 变为0 那么全部都没了
+        Map<String,String> oneItem = GetOneGood(userName,bookNum);
+        String remainNum = oneItem.get("remainNum");
+        int num = Integer.parseInt(remainNum);
+        if (num > 1)
+        {
+
+            num = num - 1;
+            oneItem.put("remainNum",String.valueOf(num));
+            StoreOneGood(userName,oneItem);
+        }
+        else{
+            DeleteOneGood(userName,bookNum);
+        }
+
+        CloseJedis();
+        return true;
+    }
+
+    //TODO 直接删除当前商品
+    public static boolean DeleteOneGood(String userName,String bookNum)
+    {
+        InitialJedis();
+
+        // 如果当前存在 userName 这个集合 那么就检查一下有没有当前商品
+        if(jedis.exists(userName))
+        {
+            // 不存在当前bookNum   操作失败
+            if(!jedis.sismember(userName,userName + bookNum))
+            {
+                return false;
+            }
+        }
+        else {
+            //如果当前不存在 userName 这个集合   操作失败
+            return false;
+        }
+
+        jedis.del(userName + bookNum);
+        jedis.srem(userName,userName + bookNum);
+        CloseJedis();
+        return true;
+    }
+
+
+    // TODO 展示一个图书商品  (用户名,书名，编号，类型，价格，ISBN，摘要，出售种类，剩余数量)
+    // TODO 返回值是一个 hashmap 需要通过 map.get() 获得相关key的value
+    public static Map<String,String> GetOneGood(String userName,String bookNum)
+    {
+        InitialJedis();
+
+        // 如果当前存在 userName 这个集合 那么就检查一下有没有当前商品
+        if(jedis.exists(userName))
+        {
+            // 不存在当前bookNum   操作失败
+            if(!jedis.sismember(userName,userName + bookNum))
+            {
+                return null;
+            }
+        }
+        else {
+            //如果当前不存在 userName 这个集合   操作失败
+            return null;
+        }
+
+        Map<String,String> map = jedis.hgetAll(userName + bookNum);
+
+        CloseJedis();
+        return map;
+    }
+
+    //endregion
+
+    //region 买家管理购物车及购买商品
+
+
+
+    //endregion
+
+    //region 商品搜索
+
+
+
+    //endregion
 
     //测试 暂时没有网页所以就用main函数测试函数功能
     public static void main(String[] args) throws Exception
     {
         ClearJedis();
+
 
     }
 }
